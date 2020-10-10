5339c341ff2114be57c24948721897dc605453a8
Felype Ganzert <39774570+FelypeGanzert@users.noreply.github.com>
2020-10-10 18:43:00 -0300

    Created CollaboratorDao

---
+++ src/main/java/model/dao/CollaboratorDao.java
@@ -0,0 +1,64 @@
+package model.dao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+
+import db.DbException;
+import model.entites.Collaborator;
+
+public class CollaboratorDao {
+	
+	EntityManager manager;
+	
+	public CollaboratorDao(EntityManager manager) {
+		// He needs a Connection to acess the database
+		this.manager = manager;
+	}
+	
+	public void insert(Collaborator collaborator) throws DbException {
+		if(manager == null) {
+			throw new DbException("DB Connection not instantiated");
+		}
+		manager.getTransaction().begin();;
+		manager.persist(collaborator);
+		manager.getTransaction().commit();
+	}
+	
+	public void update(Collaborator collaborator) throws DbException {
+		if(manager == null) {
+			throw new DbException("DB Connection not instantiated");
+		}
+		manager.getTransaction().begin();;
+		collaborator = manager.merge(collaborator);
+		manager.getTransaction().commit();
+	}
+	
+	public void deleteById(Integer id) throws DbException {
+		if(manager == null) {
+			throw new DbException("DB Connection not instantiated");
+		}
+		manager.getTransaction().begin();
+		Collaborator collaborator = manager.find(Collaborator.class, id);
+		manager.refresh(collaborator);
+		manager.remove(collaborator);
+		manager.getTransaction().commit();
+	}
+	
+	public Collaborator findById(Integer id) throws DbException {
+		if(manager == null) {
+			throw new DbException("DB Connection not instantiated");
+		}
+		return manager.find(Collaborator.class, id);
+	}
+	
+	public List<Collaborator> findAll() throws DbException{
+		if(manager == null) {
+			throw new DbException("DB Connection not instantiated");
+		}
+		TypedQuery<Collaborator> query = manager.createQuery("SELECT c FROM Colaborador c", Collaborator.class);
+		return query.getResultList();
+	}
+	
+}
