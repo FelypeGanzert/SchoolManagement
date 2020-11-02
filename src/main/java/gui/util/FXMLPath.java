039e04e56f6daa9e3ce506cb0fd9a0a5a0e37fdd
Felype Ganzert <39774570+FelypeGanzert@users.noreply.github.com>
2020-11-02 18:14:33 -0300

    Created StudentPresence screen

--- src/main/java/gui/util/FXMLPath.java
+++ src/main/java/gui/util/FXMLPath.java
@@ -44,5 +44,6 @@ public class FXMLPath {
 	public static final String CERTIFICATES_MENU = "/gui/CertificatesMenu.fxml";
 	public static final String CERTIFICATES_REQUESTS = "/gui/CertificatesRequests.fxml";
 	public static final String CERTIFICATES_ALL_STUDENTS = "/gui/CertificatesAllStudents.fxml";
+	public static final String CERTIFICATES_REQUEST_FORM = "/gui/CertificateRequestForm.fxml";
 
 }
